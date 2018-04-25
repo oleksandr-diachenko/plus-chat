7871e272a09ce0471bd6caeeefb49c325684c7f3
alexander-diachenko <rfZfynbg777>
2018-04-25 19:38:47 +0300

    читает только русский

--- src/main/java/TextToSpeech.java
+++ src/main/java/TextToSpeech.java
@@ -14,7 +14,8 @@ public class TextToSpeech {
     private SynthesiserV2 synthesizer = new SynthesiserV2("AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw");
 
     public void speak(String text) throws IOException, JavaLayerException {
-        synthesizer.setSpeed(0.4);
+        synthesizer.setSpeed(0.8);
+        synthesizer.setLanguage("ru");
         InputStream mp3Data = synthesizer.getMP3Data(text);
 
         Player player = new Player(mp3Data);
