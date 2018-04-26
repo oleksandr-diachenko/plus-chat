b1153de5470c47a84158fd070eab64d74eb2534c
alwxander.diachenko <mail.positiv@gmail.com>
2018-04-26 12:18:57 +0300

    Добавил код для тестового добавление юзера в json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
+import model.User;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
 import util.AppProperty;
 
+import java.io.*;
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 public class Main {
@@ -9,6 +15,11 @@ public class Main {
 	public static PircBotX bot;
 
 	public static void main(String[] args) throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		List<User> users = mapper.readValue(JSONParser.readFile("./users.json"), new TypeReference<List<User>>(){});
+		User user = createDummyUser();
+		users.add(user);
+		mapper.writeValue(new FileOutputStream("./users.json"), users);
 		Properties connect = AppProperty.getProperty("connect.properties");
 		Configuration config = new Configuration.Builder()
 				.setName(connect.getProperty("twitch.botname"))
@@ -20,5 +31,15 @@ public class Main {
 
 		bot = new PircBotX(config);
 		bot.startBot();
+	}
+
+	private static User createDummyUser() {
+		User user = new User();
+		user.setName("petia");
+		user.setFirstMessage(new Date());
+		user.setLastMessage(new Date());
+		user.setLevel(1);
+		user.setExp(10);
+		return user;
 	}
 }
