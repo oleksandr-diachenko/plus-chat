40784274ad1c010eada0381fbd31b4548cdb8283
alexander-diachenko <rfZfynbg777>
2018-06-26 17:57:47 +0300

    добавил тест для TimeUtil

---
+++ src/test/java/twitch/TimeUtilTest.java
@@ -0,0 +1,22 @@
+package twitch;
+
+import org.junit.Test;
+import util.TimeUtil;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Alexander Diachenko
+ */
+public class TimeUtilTest {
+
+    @Test
+    public void dateTo_dd_MM_yyyy_HH_mm() {
+        Date date = new Date(100);
+        String dateToString = TimeUtil.getDateToString(date);
+
+        assertEquals("01-01-1970 02:00", dateToString);
+    }
+}
