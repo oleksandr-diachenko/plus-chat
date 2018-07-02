a02d36ecc7288eb8e9d3b1309c4c3fb2202db9f8
alexander-diachenko <rfZfynbg777>
2018-07-02 18:42:07 +0300

    добавил лого

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import insidefx.undecorator.UndecoratorScene;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
@@ -13,6 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setAlwaysOnTop(true);
+        primaryStage.getIcons().add(new Image("/img/logo.png"));
         Region root = FXMLLoader.load(getClass().getResource("/view/chat.fxml"));
         UndecoratorScene undecorator = new UndecoratorScene(primaryStage, root);
         undecorator.setFadeInTransition();
