53b776275fe606700da3ceef0350fc2adf1aac60
alwxander.diachenko <mail.positiv@gmail.com>
2018-07-03 14:55:00 +0300

    добавил fxml, контроллер и проперти для настроек

---
+++ src/main/java/controller/SettingController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+
+/**
+ * @author Alexander Diachenko.
+ */
+public class SettingController {
+    public ChoiceBox languageChoiceBox;
+    public ChoiceBox themeChoiceBox;
+    public ChoiceBox fontChoiceBox;
+    public Slider fontSizeSlider;
+    public Slider transparencySlider;
+    public ColorPicker backgroundColorPicker;
+    public ColorPicker nickColorPicker;
+    public ColorPicker separatorColorPicker;
+    public ColorPicker messageColorPicker;
+
+    public void confirmAction() {
+    }
+
+    public void cancelAction() {
+    }
+}
