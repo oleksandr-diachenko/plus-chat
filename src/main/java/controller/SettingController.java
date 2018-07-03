e4d511d1a8b58e0dd2b4614e906a6df4a55eab7d
alwxander.diachenko <mail.positiv@gmail.com>
2018-07-03 17:26:01 +0300

    при прокручивании слайдера отображается значение

--- src/main/java/controller/SettingController.java
+++ src/main/java/controller/SettingController.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import util.AppProperty;
 
@@ -12,6 +13,10 @@ import java.util.Properties;
  * @author Alexander Diachenko.
  */
 public class SettingController {
+    @FXML
+    private Label transparencyValue;
+    @FXML
+    private Label fontSize;
     @FXML
     private ChoiceBox languageChoiceBox;
     @FXML
@@ -34,6 +39,13 @@ public class SettingController {
 
     public void initialize() {
         settings = AppProperty.getProperty("./settings/settings.properties");
+        fontSizeSlider.valueProperty().addListener((ov, old_val, new_val) -> {
+            fontSize.setText(String.valueOf(Math.round(new_val.doubleValue())));
+        });
+        transparencySlider.valueProperty().addListener((ov, old_val, new_val) -> {
+            String format = String.format("%.2f", new_val.doubleValue() / 100);
+            transparencyValue.setText(format);
+        });
     }
 
     public void confirmAction() {
--- src/main/java/util/AppProperty.java
+++ src/main/java/util/AppProperty.java
@@ -3,7 +3,9 @@ package util;
 import org.apache.log4j.Logger;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Properties;
 
 /**
@@ -23,5 +25,24 @@ public class AppProperty {
             logger.error(exception.getMessage(), exception);
         }
         return mainProperties;
+    }
+
+    public static Properties setProperties(Properties properties) {
+        OutputStream output = null;
+        try {
+            output = new FileOutputStream(".settings/settings.properties");
+            properties.store(output, null);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+        }
+        return properties;
     }
 }
