01fb5bcdb4b171ad083387e61faecc4365f72b52
alwxander.diachenko <mail.positiv@gmail.com>
2018-07-10 14:08:52 +0300

    поменял реализацию трансфера колора в хеш веб колор

--- src/main/java/chat/util/ColorUtil.java
+++ src/main/java/chat/util/ColorUtil.java
@@ -8,6 +8,9 @@ import javafx.scene.paint.Color;
 public class ColorUtil {
 
     public static String getHexColor(final Color color) {
-        return "#" + Integer.toHexString(color.hashCode()).substring(0, 6).toUpperCase();
+        return String.format("#%02X%02X%02X",
+                (int) (color.getRed() * 255),
+                (int) (color.getGreen() * 255),
+                (int) (color.getBlue() * 255));
     }
 }
