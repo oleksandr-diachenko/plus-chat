bfe3351878c614c739d147ccaddeffe4a2288b7e
alwxander.diachenko <mail.positiv@gmail.com>
2018-07-18 09:52:53 +0300

    добавил тест для директов

---
+++ src/test/java/twitch/DirectRepositoryTest.java
@@ -0,0 +1,41 @@
+package twitch;
+
+import chat.model.entity.Direct;
+import chat.model.repository.DirectRepository;
+import chat.model.repository.JSONDirectRepository;
+import org.junit.Test;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Alexander Diachenko.
+ */
+public class DirectRepositoryTest {
+
+    private DirectRepository directRepository = new JSONDirectRepository(getResource("/json/directs.json"));
+
+    @Test
+    public void getAllDirectsTest() {
+        final Set<Direct> directs = this.directRepository.getAll();
+        assertTrue(!directs.isEmpty());
+    }
+
+    @Test
+    public void getDirectByNameTest() {
+        final Optional<Direct> directByName = this.directRepository.getByName("POSITIV");
+        assertTrue(directByName.isPresent());
+    }
+
+    @Test
+    public void getDirectByIncorrectNameTest() {
+        final Optional<Direct> directByName = this.directRepository.getByName("QWE");
+        assertTrue(!directByName.isPresent());
+    }
+
+    private String getResource(final String path) {
+        return getClass().getResource(path).getPath();
+    }
+}
