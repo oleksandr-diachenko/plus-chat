c7cc8b11b0f1b73e3d2a700c65fc891ad8e4cfa0
alwxander.diachenko <mail.positiv@gmail.com>
2018-07-18 13:50:02 +0300

    Добавил fxml и контроллер для data

---
+++ src/main/java/chat/controller/DataController.java
@@ -0,0 +1,16 @@
+package chat.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * @author Alexander Diachenko.
+ */
+public class DataController {
+
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox root;
+}
