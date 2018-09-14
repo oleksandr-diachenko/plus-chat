1f7c0290be40290db1c68e6839fdea6fa8b451a3
alwxander.diachenko <mail.positiv@gmail.com>
2018-09-14 12:40:18 +0300

    рефакторинг

--- src/main/java/chat/util/FileUtil.java
+++ src/main/java/chat/util/FileUtil.java
@@ -1,8 +1,9 @@
 package chat.util;
 
 import java.io.File;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Alexander Diachenko
@@ -10,14 +11,10 @@ import java.util.Set;
 public class FileUtil {
 
     public static Set<File> getFilesFromFolder(final String path) {
-        final Set<File> result = new HashSet<>();
         final File folder = new File(path);
         final File[] files = folder.listFiles();
-        for (File file : files) {
-            if (file.isFile()) {
-                result.add(file);
-            }
-        }
-        return result;
+        return Arrays.stream(files)
+                .filter(File::isFile)
+                .collect(Collectors.toSet());
     }
 }
