9bde201dd26bfedd3b988ef0f510ccda7db8f29c
alexander.diachenko <mail.positiv@gmail.com>
2018-11-19 10:35:08 +0200

    добавил тесты для OrderCommand::execute

--- src/test/java/chat/unit/command/OrderCommandTest.java
+++ src/test/java/chat/unit/command/OrderCommandTest.java
@@ -2,28 +2,43 @@ package chat.unit.command;
 
 import chat.command.ICommand;
 import chat.command.OrderCommand;
+import chat.model.entity.User;
 import chat.model.repository.OrderRepository;
 import chat.model.repository.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Oleksandr_Diachenko
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class OrderCommandTest {
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private OrderRepository orderRepository;
-    private ICommand command = new OrderCommand(userRepository, "p0sltlv", orderRepository);
+    private ICommand command;
+
+    @Before
+    public void setup() {
+        UserRepository userRepository = mock(UserRepository.class);
+        OrderRepository orderRepository = mock(OrderRepository.class);
+        String userName = "p0sltlv";
+        when(userRepository.getUserByName(userName)).thenReturn(Optional.of(getUser()));
+        command = new OrderCommand(userRepository, userName, orderRepository);
+    }
+
+    private User getUser() {
+        User user = new User();
+        user.setName("p0sltlv");
+        user.setCustomName("POSITIV");
+        user.setFirstMessageDate("1970-01-01 12:00");
+        user.setLastMessageDate("1970-01-01 12:01");
+        user.setExp(1);
+        user.setPoints(1000);
+        return user;
+    }
 
     @Test
     public void canExecuteTest() {
@@ -62,6 +77,16 @@ public class OrderCommandTest {
     }
 
     @Test
-    public void execute() {
+    public void executeTest() {
+        command.canExecute("!order 100 order");
+        String execute = command.execute();
+        assertEquals("POSITIV, your order is accepted (order) (100 points)", execute);
+    }
+
+    @Test
+    public void execute_notEnoughPointsTest() {
+        command.canExecute("!order 10000 order");
+        String execute = command.execute();
+        assertEquals("POSITIV, you don't have enough points! (You have 1000 points)", execute);
     }
 }
