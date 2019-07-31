06dd201f673440b7b07dd04d9d852cf9a3836a7c
diacho01 <oleksandr.diachenko@kingfisher.com>
2019-07-31 13:49:21 +0300

    вынес все конфигурационные стринги из конфигурационного класса в пропертю

--- src/main/java/chat/AppConfig.java
+++ src/main/java/chat/AppConfig.java
@@ -5,55 +5,112 @@ import chat.model.repository.*;
 import chat.util.AppProperty;
 import chat.util.Paths;
 import chat.util.StyleUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class AppConfig {
+
+    @Value("${path.json.commands}")
+    private String jsonCommandsPath;
+
+    @Value("${path.json.directs}")
+    private String jsonDirectsPath;
+
+    @Value("${path.json.ranks}")
+    private String jsonRanksPath;
+
+    @Value("${path.json.smiles}")
+    private String jsonSmilesPath;
+
+    @Value("${path.json.users}")
+    private String jsonUsersPath;
+
+    @Value("${path.json.orders}")
+    private String jsonOrdersPath;
+
+    @Value("${path.settings.twitch}")
+    private String settingsTwitchPath;
+
+    @Value("${path.settings.settings}")
+    private String settingsSettingsPath;
+
+    @Value("${path.settings.commands}")
+    private String settingsCommandsPath;
+
+    @Value("${path.css.chat}")
+    private String cssChatPath;
+
+    @Value("${path.css.settings}")
+    private String cssSettingsPath;
+
+    @Value("${path.css.data}")
+    private String cssDataPath;
+
+    @Value("${path.css.confirm}")
+    private String cssConfirmPath;
+
+    @Value("${path.css.randomizer}")
+    private String cssRandomizerPath;
+
+    @Value("${path.img.chat.logo}")
+    private String imgChatLogoPath;
+
+    @Value("${path.img.chat.pinenabled}")
+    private String imgChatPinEnabledPath;
+
+    @Value("${path.img.chat.pindisabled}")
+    private String imgChatPinDisabledPath;
+
+    @Value("${path.directory.sound}")
+    private String soundDirectoryPath;
 
     @Bean
     public CommandRepository commandRepository() {
-        return new JSONCommandRepository("./data/commands.json");
+        return new JSONCommandRepository(jsonCommandsPath);
     }
 
     @Bean
     public DirectRepository directRepository() {
-        return new JSONDirectRepository("./data/directs.json");
+        return new JSONDirectRepository(jsonDirectsPath);
     }
 
     @Bean
     public RankRepository rankRepository() {
-        return new JSONRankRepository("./data/ranks.json");
+        return new JSONRankRepository(jsonRanksPath);
     }
 
     @Bean
     public SmileRepository smileRepository() {
-        return new JSONSmileRepository("./data/smiles.json");
+        return new JSONSmileRepository(jsonSmilesPath);
     }
 
     @Bean
     public UserRepository userRepository() {
-        return new JSONUserRepository("./data/users.json");
+        return new JSONUserRepository(jsonUsersPath);
     }
 
     @Bean
     public OrderRepository orderRepository() {
-        return new JSONOrderRepository("./data/orders.json");
+        return new JSONOrderRepository(jsonOrdersPath);
     }
 
     @Bean
     public AppProperty twitchProperties() {
-        return new AppProperty("./settings/twitch.properties");
+        return new AppProperty(settingsTwitchPath);
     }
 
     @Bean
     public AppProperty settingsProperties() {
-        return new AppProperty("./settings/settings.properties");
+        return new AppProperty(settingsSettingsPath);
     }
 
     @Bean
     public AppProperty commandsProperties() {
-        return new AppProperty("./settings/commands.properties");
+        return new AppProperty(jsonCommandsPath);
     }
 
     @Bean
@@ -68,8 +125,8 @@ public class AppConfig {
 
     @Bean
     public Paths paths() {
-        return new Paths("/img/chat/logo.png", settingsProperties(), "/chat.css", "/confirm.css", "/data.css",
-                "/settings.css", "/img/chat/pin-enabled.png", "/img/chat/pin-disabled.png",
-                "/settings/twitch.properties", "./sound/", "/randomizer.css");
+        return new Paths(imgChatLogoPath, settingsProperties(), cssChatPath, cssConfirmPath, cssDataPath,
+                cssSettingsPath, imgChatPinEnabledPath, imgChatPinDisabledPath,
+                settingsTwitchPath, soundDirectoryPath, cssRandomizerPath);
     }
 }
